42c3b4d021d174ceabc7602223d595f2691d4fb4
Figsups <saksatti@gmail.com>
2024-10-04 17:55:33 -0400

    create navigation stack

--- thrive/App.tsx
+++ thrive/App.tsx
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DMList from './screens/dm-pages/dm-list';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Go to messages"
+        onPress={() => navigation.navigate('Messages')}
+      />
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Messages" component={DMList} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
